433abc47e585faf9349f8232737d381edf7ddc05
Priya <Priyasapanroy@gmail.com>
2024-08-29 23:30:50 +0530

    query dashboard

--- project/app/urls.py
+++ project/app/urls.py
@@ -11,12 +11,10 @@
 # ]
 
 
-from django.contrib import admin
 from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path("login/",views.login,name='login'),
     path("query/",views.query,name='query')
--- project/app/views.py
+++ project/app/views.py
@@ -1,84 +1,35 @@
-# from django.shortcuts import render
-# from .forms import *
-# from .models import*
-# # Create your views here.
-# def home(request):
-#     # form=Ragistration()
-#     form=RegistrationForm()
-#     # return render(request,"home.html",{"form":form})
-#     if request.method=="POST":
-#         # form=Ragistration(request.POST)
-#         form=RegistrationForm(request.POST)
-        
-#         if form.is_valid():
-#             fname=form.cleaned_data["fname"]
-#             lname=form.cleaned_data["lname"]
-#             email=form.cleaned_data["email"]
-#             contact=form.cleaned_data["contact"]
-#             print(fname,lname,email,contact)
-#             user = Registration.objects.filter(email=email)
-#             if user:
-#                 msg = "Email already exit"
-#                 form = RegistrationForm()
-#                 return render(request,"home.html",{"form":form,"msg":msg})
-#             else:
-#                 form.save()
-#                 msg="Registration succesfull"
-#                 form=RegistrationForm()
-#                 return render(request,"home.html",{"form":form,"msg":msg})
-        
-#     #         # data={"fname":fname,"lname":lname,"email":email,"contact":contact}
-#     #         # RegistrationForm.object.create()
-#     #         form.save()
-# def Login(request):
-#     form=LoginForm()
-#     if request.method== 'POST':
-#             data=LoginForm(request.POST)
-#             Login_email=form.cleaned_data["email"]
-#             Login_contact=form.cleaned_data["contact"]
-#             # print(email ,contact)
-#             uder= Registration.objects.filter(email=Login_email)
-#             if user:
-#                  user = Registration.objects.get(email=Login_email)
-#                  print(user)      
-
-#     return render(request,'login.html',{'form':form})
-
-
-# def login_data(request):
-#     form=LoginForm()
-#     if request.method== 'POST':
-#             data=LoginForm(request.POST)
-#             Login_email=form.cleaned_data["email"]
-#             Login_contact=form.cleaned_data["contact"]
-#             # print(email ,contact)
-#             uder= Registration.objects.filter(email=Login_email)
-#             if user:
-#                  user = Registration.objects.get(email=Login_email)
-#                  print(user)
-
 from django.shortcuts import render
-from .forms import *
-from .models import *
+from django.http import HttpResponse
+from .forms import RegistrationForm,LoginForm,QueryForm
+from .models import StudentModel,StudentQuery
 # Create your views here.
 
 def home(request):
-    form = RegistrationForm()
-    if request.method=='POST':
-        data = RegistrationForm(request.POST)
-        if data.is_valid():
-            name=data.cleaned_data['stu_name']
-            email=data.cleaned_data['stu_email']
-            city=data.cleaned_data['stu_city']
-            contact=data.cleaned_data['stu_mobile']
-            password = data.cleaned_data['stu_password']
-            print(name,email,city,contact,password)
-            data.save()
-            msg="Registration Successfully"
-            return render(request,'home.html',{'form':form,'msg':msg})
-    else:
-        return render(request,'home.html',{'form':form})
+    form=RegistrationForm()
+    if request.method=="POST":      
+        form=RegistrationForm(request.POST)
     
+        if form.is_valid():
+            stu_name=form.cleaned_data["stu_name"]
+            stu_email=form.cleaned_data["stu_email"]
+            stu_city=form.cleaned_data["stu_city"]
+            stu_mobile=form.cleaned_data["stu_mobile"]
+            stu_password=form.cleaned_data["stu_password"]
+            print(stu_name,stu_email,stu_city,stu_mobile)
+            user = StudentModel.objects.filter(stu_email=stu_email)
+            if user:
+                msg = "Email already exit"
+                form = RegistrationForm()
+                return render(request,"home.html",{"form":form,"msg":msg})
+            else:
+                form.save()
+                msg="Registration succesfull"
+                form=RegistrationForm()
+                return render(request,"home.html",{"form":form,"msg":msg})
+            
+    else:
+        return render(request,'home.html',{"form":form})
+
 def login(request):
     form = LoginForm()
     if request.method=="POST":
@@ -106,34 +57,34 @@ def login(request):
                         'password':password
                     }
                     initial_data = {
-                        'stu_name' : name,
-                        'stu_email' : email
-                    }
-                    form1= QueryForm(initial=initial_data,)
+                                    'stu_name': name,
+                                    'stu_email': email
+                                } 
+                    form1=QueryForm(initial=initial_data)
                     return render(request,'dashboard.html',{'data':data,'query':form1})
-                else:
+            else:
                     msg = "Email & Password not matched"
                     return render(request,'login.html',{'form':form,'msg':msg})
-            else:
+        else:
                 msg = "Email not register so please register first"
                 return render(request,'login.html',{'form':form,'msg':msg})
     else:
         return render(request,'login.html',{'form':form})
     
 def query(request):
-        form = QueryForm()
-        if request.method=="POST":
-            query_data = QueryForm(request.POST)
-            if query_data.is_valid():
-                name = query_data.cleaned_data['stu_name']
-                email = query_data.cleaned_data['stu_email']
-                query = query_data.cleaned_data['stu_query']
-                # print(email,name,query)
-                query_data.save()
-        if user:
+    # return HttpResponse("hi.............")
+    form = QueryForm()
+    if request.method=="POST":
+        query_data = QueryForm(request.POST) 
+        # print(query_data)
+        if query_data.is_valid():
+            name =  query_data.cleaned_data['stu_name']
+            email = query_data.cleaned_data['stu_email']
+            query = query_data.cleaned_data['stu_query']
+            # print(email,name,query)
+            query_data.save()
             user = StudentModel.objects.get(stu_email=email)
-            # print(user.stu_password)
-            if user.stu_password==password:
+            if user:
                 name = user.stu_name
                 email = user.stu_email
                 contact = user.stu_mobile
@@ -145,10 +96,14 @@ def query(request):
                     'contact':contact,
                     'city':city,
                     'password':password
-                }
+                      }
                 initial_data = {
-                    'stu_name' : name,
-                    'stu_email' : email
-                }
-                form1= QueryForm(initial=initial_data,)
-                return render(request,'dashboard.html',{'data':data,'query':form1})
+                                'stu_name': name,
+                                'stu_email': email
+                            } 
+                form1=QueryForm(initial=initial_data)
+                data1=StudentQuery.objects.filter(stu_email=email)
+                return render(request,'dashboard.html',{'data':data,'query':form1,'data1':data1})
+
+
+
